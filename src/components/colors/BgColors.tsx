0064323baac0579a3a978c2006523dad45b961d4
Samdevtechnology <Samdevtechnology@gmail.com>
2024-08-13 19:46:13 +0100

    Added : Platforms Brand Color

---
+++ src/components/colors/BgColors.tsx
@@ -0,0 +1,18 @@
+const BgColors = {
+  YouTube: "#EE3939",
+  Stack_Overflow: "#EC7100",
+  Hashnode: "#0330D1",
+  GitHub: "#1A1A1A",
+  Frontend_Mentor: "#FFFFFF",
+  freeCodeCamp: "#302267",
+  Dev_to: "#333333",
+  Facebook: "#2442AC",
+  Codepen: "152A2F",
+  GitLab: "#EB4925",
+  LinkedIn: "#2D68FF",
+  Twitch: "#EE3FC8",
+  Twitter: "#43B7E9",
+  Codewars: "#8A1A50",
+};
+
+export default BgColors;
