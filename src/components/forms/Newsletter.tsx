104ef3f6e412c96187b34c18bc5a4e04f0518569
Samdevtechnology <Samdevtechnology@gmail.com>
2025-01-06 04:57:20 +0100

    Add Newsletter component with email subscription form and validation

---
+++ src/components/forms/Newsletter.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "../ui/form";
+import { useState } from "react";
+import { Mail } from "../icons/Icons";
+
+const newsletterSchema = z.object({
+  email: z.string().min(1, "Email can’t be empty").email(),
+});
+
+const Newsletter = () => {
+  const [subscribed, setSubscribed] = useState<boolean>(false);
+
+  const form = useForm<z.infer<typeof newsletterSchema>>({
+    resolver: zodResolver(newsletterSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const handleSubmit = async ({ email }: z.infer<typeof newsletterSchema>) => {
+    try {
+      console.log(email);
+      // const res = await addUserToEmailList(
+
+      // );
+      setSubscribed(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="mb-4">
+      {subscribed ? (
+        <div>
+          <div className="flex justify-center items-center text-lg font-semibold text-primary">
+            Subscribed. Thank You &nbsp; <Mail />
+          </div>
+          <div className="text-center text-sm text-gray-600">
+            We will send you updates about our latest projects and features.
+          </div>
+        </div>
+      ) : (
+        <Form {...form}>
+          <form className="relative" onSubmit={form.handleSubmit(handleSubmit)}>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        {...field}
+                        className="bg-white h-12 dark:text-background pr-28"
+                      />
+                    </FormControl>
+                    <FormMessage className="absolute font-semibold" />
+                  </FormItem>
+                );
+              }}
+            />
+            <Button
+              size="sm"
+              type="submit"
+              className="bg-primary w-fit absolute transform -translate-y-1/2 top-1/2 right-2"
+            >
+              Subscribe
+            </Button>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
+
+export default Newsletter;
