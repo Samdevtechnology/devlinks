26e422d5e6d2a81acff3138f69a4084785f0fad2
Samdevtechnology <Samdevtechnology@gmail.com>
2025-01-07 23:14:28 +0100

    Update forgot password email template with new security code message and styling

--- src/emails/forgotPassword.tsx
+++ src/emails/forgotPassword.tsx
@@ -17,8 +17,6 @@ interface AWSVerifyEmailProps {
   verificationCode?: string;
 }
 
-console.log("🚀 ~ baseUrl:", process.env.VERCEL_URL);
-
 // const baseUrl = process.env.VERCEL_URL
 //   ? `https://${process.env.VERCEL_URL}`
 //   : "";
@@ -30,12 +28,12 @@ export default function AWSVerifyEmail({
   return (
     <Html>
       <Head />
-      <Preview>AWS Email Verification</Preview>
+      <Preview>Password Reset Security Code</Preview>
       <Body style={main}>
         <Section style={imageSection}>
           <Img
-            src={`${baseUrl}/logo.svg`}
-            width="75"
+            src={`${baseUrl}/logo-text.svg`}
+            width="175"
             height="45"
             alt="DevTools Logo"
           />
@@ -43,43 +41,36 @@ export default function AWSVerifyEmail({
         <Container style={container}>
           <Section style={coverSection}>
             <Section style={upperSection}>
-              <Heading style={h1}>Verify your email address</Heading>
+              <Heading style={h1}>Password Reset Security Code</Heading>
+              <Text style={mainText}>Hi Love (First Name),</Text>
               <Text style={mainText}>
-                Thanks for starting the new AWS account creation process. We
-                want to make sure it&apos;s really you. Please enter the
-                following verification code when prompted. If you don&apos;t
-                want to create an account, you can ignore this message.
+                We received a request to reset your password. Use the
+                verification code below to continue.
               </Text>
-              <Section style={verificationSection}>
-                <Text style={verifyText}>Verification code</Text>
-
-                <Text style={codeText}>{verificationCode}</Text>
-                <Text style={validityText}>
-                  (This code is valid for 10 minutes)
-                </Text>
+              <Section style={codeContainer}>
+                <Heading style={codeStyle}>564873</Heading>
               </Section>
+              <Text style={mainText}>
+                This security code will be valid for 10 minutes. If you
+                didn&apos;t request a password reset, you can safely ignore this
+                email, Please do not share this code with anyone.
+              </Text>
             </Section>
             <Hr />
             <Section style={lowerSection}>
               <Text style={cautionText}>
-                Amazon Web Services will never email you and ask you to disclose
-                or verify your password, credit card, or banking account number.
+                If you have any questions or need assistance, our support team
+                is here to help.
               </Text>
             </Section>
           </Section>
           <Text style={footerText}>
-            This message was produced and distributed by Amazon Web Services,
-            Inc., 410 Terry Ave. North, Seattle, WA 98109. © 2022, Amazon Web
-            Services, Inc.. All rights reserved. AWS is a registered trademark
-            of{" "}
-            <Link href="https://amazon.com" target="_blank" style={link}>
-              Amazon.com
+            © 2024 Devlinks. All rights reserved.
+            <br />
+            Developer -{" "}
+            <Link href="https://x.com/samdevtech" target="_blank" style={link}>
+              SamdevTech,
             </Link>
-            , Inc. View our{" "}
-            <Link href="https://amazon.com" target="_blank" style={link}>
-              privacy policy
-            </Link>
-            .
           </Text>
         </Container>
       </Body>
@@ -105,6 +96,12 @@ const h1 = {
   fontSize: "20px",
   fontWeight: "bold",
   marginBottom: "15px",
+};
+
+const li = {
+  marginBottom: "15px",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
 };
 
 const link = {
@@ -143,31 +140,24 @@ const footerText = {
   padding: "0 20px",
 };
 
-const verifyText = {
-  ...text,
-  margin: 0,
-  fontWeight: "bold",
-  textAlign: "center" as const,
+const codeContainer = {
+  background: "rgba(0,0,0,.05)",
+  borderRadius: "4px",
+  margin: "16px auto 14px",
+  verticalAlign: "middle",
 };
 
-const codeText = {
-  ...text,
-  fontWeight: "bold",
-  fontSize: "36px",
-  margin: "10px 0",
+const codeStyle = {
+  color: "#000",
+  display: "inline-block",
+  paddingBottom: "8px",
+  paddingTop: "8px",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  margin: "0 auto",
+  width: "100%",
   textAlign: "center" as const,
-};
-
-const validityText = {
-  ...text,
-  margin: "0px",
-  textAlign: "center" as const,
-};
-
-const verificationSection = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
+  letterSpacing: "8px",
 };
 
 const mainText = { ...text, marginBottom: "14px" };
