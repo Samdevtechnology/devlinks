a03c5591e08824318e2c68dd6ec88b5986b672c2
Samdevtechnology <Samdevtechnology@gmail.com>
2025-01-28 05:25:27 +0100

    Enhance user view page with loading spinner, URL copying functionality, and theme toggle

--- src/app/view/[userId]/page.tsx
+++ src/app/view/[userId]/page.tsx
@@ -11,8 +11,14 @@ import {
 } from "firebase/firestore";
 import { db } from "@/stores/firebase/config"; // Adjust this path based on your setup
 import UserCard from "@/app/preview/components/UserCard";
-import { Link } from "@/types/link";
+import { Link as LinkType } from "@/types/link";
 import Container from "@/components/common/Container";
+import Link from "next/link";
+import ThemeToggleBtn from "@/components/common/ThemeToggleBtn";
+import SplitBtn from "@/app/preview/components/SplitBtn";
+import { toast } from "@/components/ui/use-toast";
+import Logo from "@/components/common/Logo";
+import LoadingSpinner from "@/components/common/LoadingSpinner";
 
 interface User {
   uid: string;
@@ -30,8 +36,32 @@ interface ViewPageProps {
 
 const ViewPage = ({ params }: ViewPageProps) => {
   const [user, setUser] = useState<User | null>(null);
-  const [links, setLinks] = useState<Link[]>([]);
+  const [links, setLinks] = useState<LinkType[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const currentUrl = window.location.href;
+      setUrl(currentUrl.replace("/preview", `/view/${user?.uid}`));
+    }
+  }, [user?.uid]);
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(url);
+      toast({
+        title: "Link Copied",
+      });
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
+  const handleFlip = () => {
+    setIsFlipped((prev) => !prev);
+  };
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -50,8 +80,8 @@ const ViewPage = ({ params }: ViewPageProps) => {
         const linksSnapshot = await getDocs(
           query(linksCollection, orderBy("order"))
         );
-        const userLinks: Link[] = linksSnapshot.docs.map((doc) => {
-          const data = doc.data() as Link;
+        const userLinks: LinkType[] = linksSnapshot.docs.map((doc) => {
+          const data = doc.data() as LinkType;
           return {
             id: doc.id,
             type: data.type,
@@ -71,14 +101,33 @@ const ViewPage = ({ params }: ViewPageProps) => {
   }, [params.userId]);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <Container className="pt-4 flex justify-center items-center h-screen">
+        <LoadingSpinner text="Fetching User" />
+      </Container>
+    );
   }
 
   return (
     <div>
       {user ? (
         <Container className="pt-4 flex flex-col h-screen">
-          <UserCard user={user} links={links} />
+          <div className="flex justify-between items-center gap-4 sm:bg-card rounded-xl py-2 sm:px-4 dark:bg-primary-light">
+            <div className="w-full sm:w-fit">
+              <Link href="/">
+                <Logo />
+              </Link>
+            </div>
+            <div className="w-full sm:w-fit flex justify-center items-center gap-2">
+              <ThemeToggleBtn />
+              <SplitBtn
+                copyFunc={copyToClipboard}
+                qrFunc={handleFlip}
+                isFlipped={isFlipped}
+              />
+            </div>
+          </div>
+          <UserCard user={user} links={links} isFlipped={isFlipped} url={url} />
         </Container>
       ) : (
         <div>User not found</div>
